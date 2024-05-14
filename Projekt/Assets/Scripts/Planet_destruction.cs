6183610f93cb9586461d235968c6978a9c4d7247
Gustav <112876953+ARadLad@users.noreply.github.com>
2024-05-14 16:45:32 +0200

    started explosion

--- Projekt/Assets/Scripts/Planet_destruction.cs
+++ Projekt/Assets/Scripts/Planet_destruction.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System;
 public class Planet_destruction : MonoBehaviour
 {
+    public GameObject explosion;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +16,29 @@ public class Planet_destruction : MonoBehaviour
      private void OnCollisionEnter(Collision collision){
         gameObject.GetComponent<Rigidbody>().mass += collision.gameObject.GetComponent<Rigidbody>().mass;
 
-        float comb_vol;
-        float new_r;
-
-        comb_vol = Convert.ToSingle(4/3f * Math.PI * Math.Pow(collision.gameObject.transform.localScale.x, 3f) +
+        float comb_vol = Convert.ToSingle(4/3f * Math.PI * Math.Pow(collision.gameObject.transform.localScale.x, 3f) +
          4/3f * Math.PI * Math.Pow(gameObject.transform.localScale.x, 3f));
         
-        new_r = Convert.ToSingle(Math.Pow(comb_vol / (Math.PI * 4/3f), 1/3f));
+        float new_r = Convert.ToSingle(Math.Pow(comb_vol / (Math.PI * 4/3f), 1/3f));
         
         gameObject.transform.localScale = new Vector3(new_r, new_r, new_r);
 
+
+        
+    
+        float dif_x = gameObject.transform.position.x - collision.gameObject.transform.position.x;
+        float dif_z = gameObject.transform.position.z -  collision.gameObject.transform.position.z;
+        float angle = Convert.ToSingle(Math.Atan2(dif_x, dif_z) * 180 / Math.PI);
+        
+        float rot_x = Convert.ToSingle(Math.Sin(angle));
+        float rot_z = Convert.ToSingle(Math.Cos(angle));
+
+        Debug.Log(angle);
+        
+        GameObject effect = Instantiate(explosion, collision.gameObject.transform.position, Quaternion.Euler(0, angle, 0));
+        
           
         Destroy(collision.gameObject);
+        
     }
 }
